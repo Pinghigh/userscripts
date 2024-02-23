1608df92be745f06b88ff4917729e838c22cb2b9
Tibrella <pinghigh24678@outlook.com>
2024-02-23 16:34:05 +0800

    Update show-luogu-mainpage.user.js

--- show-luogu-mainpage.user.js
+++ show-luogu-mainpage.user.js
@@ -3,15 +3,23 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.luogu.com.cn/user/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Tibrella
 // @description 2024/2/23 15:20:03
 // @license     MPL 2.0
 // @homepageURL https://github.com/pinghigh/userscripts
 // @downloadURL https://raw.githubusercontent.com/Pinghigh/userscripts/main/show-luogu-mainpage.user.js
 // ==/UserScript==
-document.addEventListener('readystatechange', (event) => {
+
+const displayMainPage = () => {
     var intro = document.getElementsByClassName('introduction')[0];
     intro.previousElementSibling.style.setProperty('display', 'none');
     intro.style.removeProperty('display');
+}
+
+document.addEventListener('readystatechange', (event) => {
+    displayMainPage();
+    document.getElementsByClassName('items')[0].getElementsByTagName('li')[0].addEventListener("click", function(){
+        displayMainPage();
+    });
 })
